10e737a072628359d6e7ec3705be9a42a846579e
Daniel Lee <DLee@Daniels-MBP.home>
2018-06-24 11:43:04 -0400

    initialize

---
+++ Progress_Bar_Example/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Progress_Bar_Example
+//
+//  Created by Daniel Lee on 6/24/18.
+//  Copyright © 2018 DLEE. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        self.view.backgroundColor = .yellow
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
