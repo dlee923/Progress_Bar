37990a7a99f3818efc002991a2dd0793630a9e83
Daniel Lee <DLee@Daniels-MBP.home>
2018-06-26 21:39:33 -0400

    Added percentage text and download status

--- Progress_Bar_Example/ViewController.swift
+++ Progress_Bar_Example/ViewController.swift
@@ -12,27 +12,41 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
 
     let shapeLayer = CAShapeLayer()
     let trackLayer = CAShapeLayer()
+    let progressPct: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 35, weight: .bold)
+        label.textColor = .purple
+        return label
+    }()
+    let progressStatus: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .center
+        label.font = UIFont.systemFont(ofSize: 10, weight: .bold)
+        label.textColor = .purple
+        return label
+    }()
+    let progressContainer = UILabel()
+    let containerDimension: CGFloat = 100
+    let progressStatusPct: CGFloat = 0.8
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
-        print("download finished")
-    }
     
-    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
-        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
-        print(percentage)
-    }
+    let urlString = "https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg"
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = .yellow
         
-        let circularPath = UIBezierPath(arcCenter: view.center, radius: 100, startAngle: -(CGFloat.pi / 2), endAngle: 2 * CGFloat.pi, clockwise: true)
+        add_downloading_labels()
+        
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         
         trackLayer.path = circularPath.cgPath
         trackLayer.strokeColor = UIColor.black.cgColor.copy(alpha: 0.5)
         trackLayer.lineWidth = 5
         trackLayer.fillColor = UIColor.clear.cgColor
+        trackLayer.position = self.view.center
         
         self.view.layer.addSublayer(trackLayer)
         
@@ -43,13 +57,29 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
         shapeLayer.strokeEnd = 0
         shapeLayer.fillColor = UIColor.clear.cgColor
+        shapeLayer.position = self.view.center
+        shapeLayer.transform = CATransform3DMakeRotation(-(CGFloat.pi / 2), 0, 0, 1)
         
         self.view.layer.addSublayer(shapeLayer)
         
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
     }
     
-    let urlString = "https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg"
+    fileprivate func add_downloading_labels() {
+        progressContainer.frame = CGRect(x: 0, y: 0, width: containerDimension, height: containerDimension)
+        progressPct.frame = CGRect(x: 0, y: 0, width: containerDimension, height: containerDimension * progressStatusPct)
+        progressStatus.frame = CGRect(x: 0, y: containerDimension * progressStatusPct, width: containerDimension, height: containerDimension * (1-progressStatusPct))
+        progressContainer.addSubview(progressPct)
+        progressContainer.addSubview(progressStatus)
+        self.view.addSubview(progressContainer)
+        progressContainer.center = self.view.center
+        
+        progressPct.textAlignment = .center
+        progressStatus.textAlignment = .center
+        
+        progressPct.text = "0%"
+        progressStatus.text = "standby..."
+    }
     
     fileprivate func animateProgressBar() {
         animate_layer(keyPath: "strokeEnd", toValue: 1, duration: 1, forKey: "animate_stroke", itemToAnimate: self.shapeLayer)
@@ -69,6 +99,10 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     }
     
     private func downloadFile() {
+        self.shapeLayer.strokeEnd = 0
+        progressPct.text = "0%"
+        progressStatus.text = "downloading..."
+        
         let config = URLSessionConfiguration.default
         let operation = OperationQueue()
         let urlSession = URLSession(configuration: config, delegate: self, delegateQueue: operation)
@@ -76,6 +110,29 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         guard let url = URL(string: urlString) else { return }
         let downloadTask = urlSession.downloadTask(with: url)
         downloadTask.resume()
+        
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        print("download finished")
+        DispatchQueue.main.async {
+            self.progressStatus.text = "finished"
+        }
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
+        
+        let rounded_pct = Int(percentage * 100)
+        
+        DispatchQueue.main.async {
+            self.shapeLayer.strokeEnd = percentage
+            self.shapeLayer.lineWidth = percentage * 10 < 5 ? 5 : percentage * 10
+            self.trackLayer.lineWidth = percentage * 10 < 5 ? 5 : percentage * 10
+            self.progressPct.text = "\(rounded_pct)%"
+        }
+        
+        print(percentage)
     }
     
     @objc private func handleTap() {
@@ -83,7 +140,7 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
         downloadFile()
         
-        animateProgressBar()
+//        animateProgressBar()
     }
     
     override func didReceiveMemoryWarning() {
