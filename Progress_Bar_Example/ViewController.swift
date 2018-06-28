c5468e1b5ce040a397415c297b92ba4b39ad6729
Daniel Lee <DLee@Daniels-MBP.home>
2018-06-27 21:56:16 -0400

    refactored code.  added pulsating action

--- Progress_Bar_Example/ViewController.swift
+++ Progress_Bar_Example/ViewController.swift
@@ -10,8 +10,10 @@ import UIKit
 
 class ViewController: UIViewController, URLSessionDownloadDelegate {
 
-    let shapeLayer = CAShapeLayer()
-    let trackLayer = CAShapeLayer()
+    var shapeLayer: CAShapeLayer!
+    var trackLayer: CAShapeLayer!
+    var pulseLayer: CAShapeLayer!
+    
     let progressPct: UILabel = {
         let label = UILabel()
         label.textAlignment = .center
@@ -29,7 +31,8 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     let progressContainer = UILabel()
     let containerDimension: CGFloat = 100
     let progressStatusPct: CGFloat = 0.8
-    
+    let trackWidthStart: CGFloat = 8
+    let trackWidthEnd: CGFloat = 12
     
     let urlString = "https://upload.wikimedia.org/wikipedia/commons/3/3d/LARGE_elevation.jpg"
     
@@ -38,31 +41,39 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         // Do any additional setup after loading the view, typically from a nib.
         self.view.backgroundColor = .yellow
         
+        pulseLayer = createCircularLayer(fillColor: UIColor.purple.withAlphaComponent(0.85), strokeColor: .clear)
+        let pulseAnimation = CABasicAnimation(keyPath: "transform.scale")
+        pulseAnimation.toValue = 1.2
+        pulseAnimation.duration = 0.8
+        pulseAnimation.timingFunction = CAMediaTimingFunction(name: kCAMediaTimingFunctionEaseInEaseOut)
+        pulseAnimation.autoreverses = true
+        pulseAnimation.repeatCount = Float.infinity
+        pulseLayer.add(pulseAnimation, forKey: "pulse")
+        self.view.layer.addSublayer(pulseLayer!)
+        
+        trackLayer = createCircularLayer(fillColor: .yellow, strokeColor: .lightGray)
+        self.view.layer.addSublayer(trackLayer!)
+        
+        shapeLayer = createCircularLayer(fillColor: .clear, strokeColor: .purple)
+        shapeLayer.strokeEnd = 0
+        self.view.layer.addSublayer(shapeLayer!)
+        
         add_downloading_labels()
         
-        let circularPath = UIBezierPath(arcCenter: .zero, radius: 100, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        
-        trackLayer.path = circularPath.cgPath
-        trackLayer.strokeColor = UIColor.black.cgColor.copy(alpha: 0.5)
-        trackLayer.lineWidth = 5
-        trackLayer.fillColor = UIColor.clear.cgColor
-        trackLayer.position = self.view.center
-        
-        self.view.layer.addSublayer(trackLayer)
-        
-        shapeLayer.path = circularPath.cgPath
-        shapeLayer.strokeColor = UIColor.purple.cgColor
-        shapeLayer.lineWidth = 5
-        shapeLayer.lineCap = kCALineCapRound
-        
-        shapeLayer.strokeEnd = 0
-        shapeLayer.fillColor = UIColor.clear.cgColor
-        shapeLayer.position = self.view.center
-        shapeLayer.transform = CATransform3DMakeRotation(-(CGFloat.pi / 2), 0, 0, 1)
-        
-        self.view.layer.addSublayer(shapeLayer)
-        
         self.view.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleTap)))
+    }
+    
+    private func createCircularLayer(fillColor: UIColor, strokeColor: UIColor) -> CAShapeLayer {
+        let layer = CAShapeLayer()
+        let circularPath = UIBezierPath(arcCenter: .zero, radius: 75, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        layer.path = circularPath.cgPath
+        layer.transform = CATransform3DMakeRotation(-(CGFloat.pi / 2), 0, 0, 1)
+        layer.strokeColor = strokeColor.cgColor
+        layer.lineCap = kCALineCapRound
+        layer.lineWidth = trackWidthStart
+        layer.fillColor = fillColor.cgColor
+        layer.position = self.view.center
+        return layer
     }
     
     fileprivate func add_downloading_labels() {
@@ -81,10 +92,11 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         progressStatus.text = "standby..."
     }
     
+    /*
     fileprivate func animateProgressBar() {
         animate_layer(keyPath: "strokeEnd", toValue: 1, duration: 1, forKey: "animate_stroke", itemToAnimate: self.shapeLayer)
-        animate_layer(keyPath: "lineWidth", toValue: 10, duration: 0.75, forKey: "animate_width", itemToAnimate: self.shapeLayer)
-        animate_layer(keyPath: "lineWidth", toValue: 10, duration: 0.75, forKey: "animate_width", itemToAnimate: self.trackLayer)
+        animate_layer(keyPath: "lineWidth", toValue: 15, duration: 0.75, forKey: "animate_width", itemToAnimate: self.shapeLayer)
+        animate_layer(keyPath: "lineWidth", toValue: 15, duration: 0.75, forKey: "animate_width", itemToAnimate: self.trackLayer)
     }
     
     func animate_layer(keyPath: String, toValue: Double, duration: Double, forKey: String, itemToAnimate: CAShapeLayer) {
@@ -97,7 +109,8 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         
         itemToAnimate.add(basicAnimation, forKey: forKey)
     }
-    
+    */ 
+ 
     private func downloadFile() {
         self.shapeLayer.strokeEnd = 0
         progressPct.text = "0%"
@@ -110,7 +123,6 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         guard let url = URL(string: urlString) else { return }
         let downloadTask = urlSession.downloadTask(with: url)
         downloadTask.resume()
-        
     }
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
@@ -122,13 +134,12 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
     
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         let percentage = CGFloat(totalBytesWritten) / CGFloat(totalBytesExpectedToWrite)
-        
         let rounded_pct = Int(percentage * 100)
         
         DispatchQueue.main.async {
             self.shapeLayer.strokeEnd = percentage
-            self.shapeLayer.lineWidth = percentage * 10 < 5 ? 5 : percentage * 10
-            self.trackLayer.lineWidth = percentage * 10 < 5 ? 5 : percentage * 10
+            self.shapeLayer.lineWidth = percentage * self.trackWidthEnd < self.trackWidthStart ? self.trackWidthStart : percentage * self.trackWidthEnd
+            self.trackLayer.lineWidth = percentage * self.trackWidthEnd < self.trackWidthStart ? self.trackWidthStart : percentage * self.trackWidthEnd
             self.progressPct.text = "\(rounded_pct)%"
         }
         
@@ -139,8 +150,6 @@ class ViewController: UIViewController, URLSessionDownloadDelegate {
         print("download file")
         
         downloadFile()
-        
-//        animateProgressBar()
     }
     
     override func didReceiveMemoryWarning() {
